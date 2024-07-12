61fc7f105c8ed46579d38aafadf63f696be5610d
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-07-12 14:38:14 +0300

    Another test3

--- en/_data/metadata.js
+++ en/_data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Eleventy Base Blog v8 EN",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		url: "https://example.com/about-me/",
+	},
+};
